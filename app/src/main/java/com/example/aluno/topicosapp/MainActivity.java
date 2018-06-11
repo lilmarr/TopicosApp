0df62db066b130ee51aa779854eeac840c6cb41c
aluno <marisdgo@gmail.com>
2018-06-11 09:57:01 -0300

    criação da tela 2 função para abrir

--- app/src/main/java/com/example/aluno/topicosapp/MainActivity.java
+++ app/src/main/java/com/example/aluno/topicosapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.aluno.topicosapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTela2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTela2 = (Button) findViewById(R.id.buttonTela2);
+
+        btnTela2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //O que fazer quando clicar neste botão
+
+                startActivity(new Intent(MainActivity.this, Tela2Activity.class));
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/aluno/topicosapp/Tela2Activity.java
@@ -0,0 +1,57 @@
+package com.example.aluno.topicosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Tela2Activity extends AppCompatActivity {
+
+    private EditText editTextVa;
+    private EditText editTextCot;
+    private Button buttonCal;
+    private TextView textViewResultado;
+    private String finalizado;
+    private Double valor;
+    private Double cotação;
+    private Double resultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela2);
+
+        editTextVa = (EditText) findViewById(R.id.editTextVa);
+        editTextCot = (EditText) findViewById(R.id.editTextCot);
+        textViewResultado = (TextView) findViewById(R.id.textViewResultado);
+        buttonCal = (Button) findViewById(R.id.buttonCal);
+
+
+        buttonCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                valor = Double.parseDouble(editTextVa.getText().toString());
+                cotação = Double.parseDouble(editTextCot.getText().toString());
+
+
+                /*
+
+                resultado = (peso/(altura*altura));
+
+                finalizado = Double.toString(resultado);
+
+                textViewResultado.setText(finalizado);
+         */
+
+
+        }
+
+   });
+}
+    }
+
